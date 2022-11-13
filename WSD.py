884092ba107276197ff528a9084f63bc75443aa9
mjaratli <maryamjaratli@hotmail.com>
2022-11-13 14:37:57 -0500

    Training set with ranges gathering counts complete, test set next step

--- WSD.py
+++ WSD.py
@@ -14,6 +14,10 @@ def process_file(fileName):
     instances = 0
     # The ranges for each fold stored in a list
     ranges = []
+    # Boolean to check if the line is the line with the sentence
+    sentenceLine = False
+    # All the sentences in the file
+    sentences = []
     # Opening the file
     with open(fileName, 'r') as inputFile:
         # Grabbing each line
@@ -24,6 +28,15 @@ def process_file(fileName):
                 # Adds a sense to the senses list if it does not already exist
                 if sense not in senses:
                     senses.append(sense)
+                    # Controls the boolean that determines if the line with the sentence is next
+            if line.startswith("<context>"):
+                sentenceLine = True
+                continue
+            # If the sentence line is encountered, we gather the word counts per sense
+            if sentenceLine:
+                sentenceLine = False
+                sentences.append(line)
+
                 # Counts the number of instances
                 instances += 1
     # Determines the fold ranges for all five folds
@@ -35,52 +48,11 @@ def process_file(fileName):
     ranges.append(instances - (ranges[0] + ranges[1] + ranges[2] + ranges[3]))
 
     # Returns the word, the possible senses, and the ranges for all the folds
-    return word, senses, ranges
-
-
-def define_folds(fileName, ranges, word):
-    # First fold
-    foldOneidTest = []
-    foldOneidTrain = []
-    startROne = 0
-    endROne = startROne + ranges[0]
-    # Second fold
-    foldTwoidTest = []
-    foldTwoidTrain = []
-    startRTwo = ranges[0]
-    endRTwo = startRTwo + ranges[1]
-    # Third fold
-    foldThreeidTest = []
-    foldThreeidTrain = []
-    startRThree = ranges[0] + ranges[1]
-    endRThree = startRThree + ranges[2]
-    # Fourth fold
-    foldFouridTest = []
-    foldFouridTrain = []
-    startRFour = ranges[0] + ranges[1] + ranges[2]
-    endRFour = startRFour + ranges[3]
-    # Fifth fold
-    foldFiveidTest = []
-    foldFiveidTrain = []
-    startRFive = ranges[0] + ranges[1] + ranges[2] + ranges[3]
-    endRFive = startRFive + ranges[4]
-
-    count = 0
-    with open(fileName, 'r') as inputFile:
-        # Grabbing each line
-        for line in inputFile:
-            # Checks if the line begins with answer (since these lines have the sense of the word)
-            if line.startswith("<answer"):
-
-                identificationNumber = line[line.find(word + '.') + len(word) + 1:line.find('" senseid')]
-                # Grabs the id for each instance
-                if startROne <= count < endROne:
-                    foldOneidTest.append(identificationNumber)
-            count += 1
+    return word, senses, ranges, sentences
 
 
 # Grab all the necessary counts from the training sections of the file per fold
-def process_train(fileName, word, senses, ranges, startRange, endRange):
+def process_train(fileName, word, senses, ranges, startRange, endRange, sentences):
     # Counts of each sense
     sensesDict = collections.Counter()
     sensesDict[senses[0]] = 0
@@ -91,32 +63,30 @@ def process_train(fileName, word, senses, ranges, startRange, endRange):
     senseTwoWord = collections.Counter()
 
     # Count of unique words given a sense (use len() function from senseOneWord and senseTwoWord dictionaries later on
-    senseOneUniqueWords = 0
-    senseTwoUniqueWords = 0
+    sensesUniqueDict = collections.Counter()
 
     # Default punctuation list from the string class, to remove punctuation from words
     punctuation_list = list(string.punctuation)
 
-    # Boolean to check if the line is the line with the sentence
-    sentenceLine = False
+    # Counter to determine which line we are currently on
+    counter = -1
+
     with open(fileName, 'r') as inputFile:
         # Grabbing each line
         for line in inputFile:
             # Checks if the line begins with answer (since these lines have the sense of the word)
             if line.startswith("<answer"):
+                # Each time answer is encountered it is a new instance, so we add to counter here
+                counter += 1
+                if startRange <= counter < endRange:
+                    continue
                 # Grabs the sense for each instance
                 sense = line[line.find(word + '%') + len(word) + 1:line.find('"/>')]
                 # Adds sense to dictionary counter
                 sensesDict[sense] += 1
-            # Controls the boolean that determines if the line with the sentence is next
-            if line.startswith("<context>"):
-                sentenceLine = True
-                continue
-            # If the sentence line is encountered, we gather the word counts per sense
-            if sentenceLine:
-                sentenceLine = False
-                # Split line by space delimiter into an array
-                eachLine = line.split()
+                # Grabs the line from the sentences array based on counter
+                # Also splits line by space delimiter into an array
+                eachLine = sentences[counter].split()
                 for item in eachLine:
                     # Remove punctuation from words
                     for element in punctuation_list:
@@ -134,14 +104,13 @@ def process_train(fileName, word, senses, ranges, startRange, endRange):
         del senseTwoWord['head' + word + 'head']
 
         # Count of unique words per sense to use for add one smoothing later on
-        senseOneUniqueWords = len(senseOneWord)
-        senseTwoUniqueWords = len(senseTwoWord)
+        sensesUniqueDict[senses[0]] = len(senseOneWord)
+        sensesUniqueDict[senses[1]] = len(senseTwoWord)
 
-    systemP = []
-    return systemP
+    return sensesDict, senseOneWord, senseTwoWord, sensesUniqueDict
 
 
-def naivebayes():
+def naivebayes(sensesDict, sensesOneWord, sensesTwoWord, sensesUniqueDict):
     hi = 5
 
 
@@ -152,8 +121,7 @@ if len(sys.argv) >= 2:
     arg = sys.argv[1]
 
 # Process the input file
-wordMain, sensesMain, rangesM = process_file(arg)
-define_folds(arg, rangesM, wordMain)
+wordMain, sensesMain, rangesM, sentenceList = process_file(arg)
 for foldm in range(len(rangesM)):
     startRm = 0
     endRm = 0
@@ -172,4 +140,6 @@ for foldm in range(len(rangesM)):
     elif foldm == 4:
         startRm = rangesM[0] + rangesM[1] + rangesM[2] + rangesM[3]
         endRm = startRm + rangesM[foldm]
-    systemPrediction = process_train(arg, wordMain, sensesMain, rangesM, startRm, endRm)
+    sensesDictM, sensesOneWordM, sensesTwoWordM, sensesUniqueDictM = \
+        process_train(arg, wordMain, sensesMain, rangesM, startRm, endRm, sentenceList)
+
